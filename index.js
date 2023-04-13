14ad6435e800baf8f8f3819f0f2d0c8faabcb16e
Vigneshk5 <vigneshpatali5@gmail.com>
2023-04-13 20:55:54 +0530

    json res /outfits

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const { v4: uuid } = require("uuid");
 
 const app = express();
 
+app.get("/outfit", (req, res) => {
+  const tops = ["blue", "green"];
+  const pants = ["pink", "black", "orange"];
+  const hat = ["red", "green"];
+  res.json({
+    top: _.sample(tops),
+    pants: _.sample(pants),
+    hats: _.sample(hat),
+  });
+});
+
 app.listen(3000, () => console.log("server is running..."));
