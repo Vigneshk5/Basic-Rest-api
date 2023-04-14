0263b3e58ba2878ce0d7648fbff6f797790eb19a
Vigneshk5 <vigneshpatali5@gmail.com>
2023-04-14 10:57:35 +0530

    post comment

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ const _ = require("lodash");
 const { v4: uuid } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
 app.get("/outfit", (req, res) => {
   const tops = ["blue", "green"];
   const pants = ["pink", "black", "orange"];
   const hat = ["red", "green"];
+
   res.json({
     top: _.sample(tops),
     pants: _.sample(pants),
     hats: _.sample(hat),
   });
+});
+
+app.post("/comment", async (res, req) => {
+  const id = uuid();
+  const content = res.body.content;
+  if (!content) {
+    return console.log("error");
+  } else {
+    console.log(content);
+  }
+  await fs.mkdir("data/comment", { recursive: true });
+  await fs.writeFile(`data/comment/${id}.txt`, content);
 });
 
 app.listen(3000, () => console.log("server is running..."));
