ca17d70520b7ffba9a149f04f118caa1e7fe2798
Vigneshk5 <vigneshpatali5@gmail.com>
2023-04-14 11:30:46 +0530

    finished

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { v4: uuid } = require("uuid");
 const app = express();
 app.use(express.json());
 
+
 app.get("/outfit", (req, res) => {
   const tops = ["blue", "green"];
   const pants = ["pink", "black", "orange"];
@@ -18,6 +19,20 @@ app.get("/outfit", (req, res) => {
     top: _.sample(tops),
     pants: _.sample(pants),
     hats: _.sample(hat),
+  });
+});
+
+app.get("/comment/id:", async (res, req) => {
+  const id = req.params.id;
+  let content;
+  try {
+    content = await fs.readFile(`data/comment/${id}.txt`, "utf-8");
+  } catch (err) {
+    return console.log("not found");
+  }
+
+  res.json({
+    content: content,
   });
 });
 
@@ -31,6 +46,10 @@ app.post("/comment", async (res, req) => {
   }
   await fs.mkdir("data/comment", { recursive: true });
   await fs.writeFile(`data/comment/${id}.txt`, content);
+
+  res.status(201).json({
+    id: id,
+  });
 });
 
 app.listen(3000, () => console.log("server is running..."));
